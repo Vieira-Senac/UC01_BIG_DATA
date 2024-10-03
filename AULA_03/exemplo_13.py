e0fb250e62e633fc2da4d66e9e4116ead211d668
Vieira-Senac <alessandro.vieira.senac@gmail.com>
2024-10-02 21:28:48 -0300

    Add files via upload

---
+++ AULA_03/exemplo_13.py
@@ -0,0 +1,6 @@
+n1 = int(input("Informe o Primeiro Valor Inteiro"))
+n2 = int(input("Informe o Segundo Valor Inteiro"))
+if n1 == n2:
+    print(f"Os valores são iguais, então o produto é {n1*n2}")
+else:
+    print(f"Os valores são diferentes, então a soma é {n1+n2}")
