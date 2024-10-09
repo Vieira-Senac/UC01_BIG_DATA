bb96e91b2495ee61e0e51563101d2794af5d52a0
Vieira-Senac <alessandro.vieira.senac@gmail.com>
2024-10-08 21:23:28 -0300

    Add files via upload

--- AULA_06/exemplo_24.py
+++ AULA_06/exemplo_24.py
@@ -3,7 +3,11 @@ senhas = ["123","345","567","@@@$$$","8910"]
 usuario = input("Informe o nome de acesso ao sistema: ")
 for i in range(len(usuarios)):
     if usuarios[i] == usuario:
-        resp = "Usuário Encontrado"
+        senha = input("Informe a senha de acesso: ")
+        if senhas[i] == senha:
+            resp = "Acesso Liberado"
+        else:
+            resp = "Senha não Confere"
         break
     else:
         resp = "Usuário não Encontrado"
